4f5df49d1d76e006e57b1c0f11db3c662194b564
Juliana Assis <julianaaas404@gmail.com>
2023-10-03 13:31:18 -0400

    j

--- app/database/Connection.php
+++ app/database/Connection.php
@@ -18,7 +18,7 @@ class Connection
         $host         =   "localhost";
         $db_name      =   "tcc";
         $user         =   "root";
-        $pass         =   "sucesso";
+        $pass         =   "";
         $charset      =   "utf8";
         $db_driver    =   "mysql";
         $port         =   3306;
